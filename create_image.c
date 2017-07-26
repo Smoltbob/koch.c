0b07f320e14876aa1cc8488919c17fe739cce64a
Jules Simon <jules.simon@grenoble-inp.org>
2017-07-26 10:40:57 +0200

    English traduction

--- create_image.c
+++ create_image.c
@@ -1,45 +1,47 @@
 /*
- * Fichier : create_image.c
- * Description : Creation d'un fichier ppm a partir d'une image sous la forme
- *               d'un tableau uint32_t *
+ * File : create_image.c
+ * Description : creation of a ppm file from an uint32_t array representing
+ * an image.
  */
 #include "create_image.h"
 #include <stdio.h>
 
-struct couleur
+struct color
 {
     uint8_t R;
     uint8_t G;
     uint8_t B;
 };
 
-struct couleur int32tocouleur(uint32_t entier)
+/* Takes a color coded as xxRRGGBB in an uint32_t and returns the individual
+ * components */
+struct color int32tocolor(uint32_t number)
 {
-    struct couleur res;
-    /* On utilise des masques pour extraire les composantes RGB */
-    res.R = (entier >> 16) & 0xFF;
-    res.G = (entier >> 8) & 0xFF;
-    res.B = entier & 0xFF;
+    struct color res;
+    /* We use masks to extract RGB components */
+    res.R = (number >> 16) & 0xFF;
+    res.G = (number >> 8) & 0xFF;
+    res.B = number & 0xFF;
     return res;
 }
 
+/* Generates a ppm (P6: color, binary) image of size x*y from an array */
 void create_image_ppm(uint32_t *pic, int32_t size_x, int32_t size_y, char *filename)
 {
-    /* Création du fichier */
+    /* Creating file */
     FILE *fic = fopen(filename, "w");
-    /* Ecriture de l'en-tête ppm */
-    fprintf(fic, "P6\n");                                                      
-    fprintf(fic, "%d %d\n", size_x, size_y);                       
-    fprintf(fic, "%i\n", 255); 
-    /* Génération de l'image */
+    /* Writing ppm header */
+    fprintf(fic, "P6\n");
+    fprintf(fic, "%d %d\n", size_x, size_y);
+    fprintf(fic, "%i\n", 255);
+    /* Generating image */
     uint32_t *i32pixel = pic;
-    /* On boucle sur tous les pixels */
+    /* We loop through all the pixels */
     for (int32_t i = 0; i < size_x * size_y; i++)
     {
-        /* On récupère les couleurs et on écrit le pixel en binaire */
-        struct couleur pixel = int32tocouleur(*i32pixel);
-        uint8_t couleurs[] = {pixel.R, pixel.G, pixel.B};
-        fwrite(couleurs, 1, 3, fic);
+        struct color pixel = int32tocolor(*i32pixel);
+        uint8_t colors[] = {pixel.R, pixel.G, pixel.B};
+        fwrite(colors, 1, 3, fic);
         i32pixel++;
     }
     fclose(fic);
